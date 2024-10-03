efc3e7aad2cf4365eacd30310426dd1cb11a1a1e
yourusername <youremail@>
2024-10-03 13:30:33 -0600

    homework made

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,59 @@
 using System;
 
-class Program
+public class Fraction
 {
-    static void Main(string[] args)
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
     {
-        Console.WriteLine("Hello Learning03 World!");
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create instances of Fraction
+        Fraction fraction1 = new Fraction();          
+        Fraction fraction2 = new Fraction(1);        
+        Fraction fraction3 = new Fraction(5);        
+        Fraction fraction4 = new Fraction(3, 4);     
+        Fraction fraction5 = new Fraction(1, 3);     
+
+        // Print results
+        
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+        Console.WriteLine(fraction5.GetFractionString());
+        Console.WriteLine(fraction5.GetDecimalValue());
     }
 }
